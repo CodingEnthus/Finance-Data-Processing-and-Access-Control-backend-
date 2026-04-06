e6c673085f05b1136af85d4067e2298ca9a9dcdd
CodingEnthus <samratd2005@gmail.com>
2026-04-06 14:44:18 +0530

    Fixed missing checkRole import

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { createUser, getUsers, deleteUser} = require('../controllers/userController');
-
+const { checkRole } = require('../middleware/authMiddleware');
 router.post('/', createUser);
 router.get('/', getUsers);
 router.delete('/:id',checkRole(['admin']),deleteUser);
